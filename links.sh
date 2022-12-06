95503becd2116a2b04f9ed854dc55199bf0e8280
James Trousdale <james.n.trousdale@lmco.com>
2022-12-06 13:41:41 -0500

    added fancyzopnes

--- links.sh
+++ links.sh
@@ -1,5 +1,22 @@
-ln -s /c/dev/command-console/configs/lf/lfrc ~/AppData/Local/lf/lfrc
-ln -s /c/dev/command-console/configs/lf/colors ~/AppData/Local/lf/colors
-ln -s /c/dev/command-console/configs/lf/icons ~/AppData/Local/lf/icons
+## CMD elevated user:
+### lf
+mklink c:\Users\e371975\AppData\Local\lf\lfrc c:\dev\command-console\configs\lf\lfrc
+mklink c:\Users\e371975\AppData\Local\lf\colors c:\dev\command-console\configs\lf\colors
+mklink c:\Users\e371975\AppData\Local\lf\icons c:\dev\command-console\configs\lf\icons
 
-ln -s /c/dev/command-console/configs/firefox/userChrome.css ~/AppData/Roaming/Mozilla/Firefox/Profiles/fddsy9rv.default-esr/chrome/userChrome.css
+### firefox
+mklink c:\Users\e371975\AppData\Roaming\Mozilla\Firefox\Profiles\fddsy9rv.default-esr\chrome\userChrome.css c:\dev\command-console\configs\firefox\userChrome.css
+
+### starship
+mklink c:\Users\e371975\.config\starship.toml c:\dev\command-console\configs\starship\starship.toml
+
+### powetoys
+mklink c:\Users\e371975\AppData\Local\Microsoft\PowerToys\settings.json c:\dev\command-console\configs\powertoys\settings.json
+
+mklink c:\Users\e371975\AppData\Local\Microsoft\PowerToys\FancyZones\settings.json c:\dev\command-console\configs\powertoys\FancyZones\settings.json
+mklink c:\Users\e371975\AppData\Local\Microsoft\PowerToys\FancyZones\applied-layouts.json c:\dev\command-console\configs\powertoys\FancyZones\applied-layouts.json
+mklink c:\Users\e371975\AppData\Local\Microsoft\PowerToys\FancyZones\custom-layouts.json c:\dev\command-console\configs\powertoys\FancyZones\custom-layouts.json
+mklink c:\Users\e371975\AppData\Local\Microsoft\PowerToys\FancyZones\layout-hotkeys.json c:\dev\command-console\configs\powertoys\FancyZones\layout-hotkeys.json
+mklink c:\Users\e371975\AppData\Local\Microsoft\PowerToys\FancyZones\layout-templates.json c:\dev\command-console\configs\powertoys\FancyZones\layout-templates.json
+
+### vs code
