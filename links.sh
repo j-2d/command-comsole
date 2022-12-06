d1d7bcd18b2b2094464ecd537bcfed151565ffcb
James Trousdale <james.n.trousdale@lmco.com>
2022-12-06 12:53:30 -0500

    symlink userchorme

--- links.sh
+++ links.sh
@@ -1,3 +1,5 @@
 ln -s /c/dev/command-console/configs/lf/lfrc ~/AppData/Local/lf/lfrc
 ln -s /c/dev/command-console/configs/lf/colors ~/AppData/Local/lf/colors
 ln -s /c/dev/command-console/configs/lf/icons ~/AppData/Local/lf/icons
+
+ln -s /c/dev/command-console/configs/firefox/userChrome.css ~/AppData/Roaming/Mozilla/Firefox/Profiles/fddsy9rv.default-esr/chrome/userChrome.css
