4ff28fc28039ee7a453cf6052a1d17339412e6d2
James Trousdale <james.n.trousdale@lmco.com>
2022-12-06 12:49:22 -0500

    add symlic documentation and lf

---
+++ links.sh
@@ -0,0 +1,3 @@
+ln -s /c/dev/command-console/configs/lf/lfrc ~/AppData/Local/lf/lfrc
+ln -s /c/dev/command-console/configs/lf/colors ~/AppData/Local/lf/colors
+ln -s /c/dev/command-console/configs/lf/icons ~/AppData/Local/lf/icons
